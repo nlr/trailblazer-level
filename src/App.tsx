9ed6ef5ce54f3238011685a907365558a3d6e79b
nlr <nlr@users.noreply.github.com>
2023-10-07 16:34:14 +0500

    Fix link

--- src/App.tsx
+++ src/App.tsx
@@ -302,7 +302,7 @@ function App() {
 
         <div className="flex flex-col space-y-4">
           <a
-            href="htt ps://github.com/nlr/trailblazer-level/issues"
+            href="https://github.com/nlr/trailblazer-level/issues"
             target="_blank"
             className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
           >
