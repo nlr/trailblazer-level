7047b6c0466c83c9179d33f4a1a40c97ac495e93
nlr <nlr@users.noreply.github.com>
2023-12-04 01:11:48 +0500

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -302,26 +302,6 @@ function App() {
 
         <div className="flex flex-col space-y-4">
           <a
-            href="https://github.com/nlr/trailblazer-level/issues"
-            target="_blank"
-            className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
-          >
-            <span className="w-full">Report a Bug or Share an Idea</span>
-            <svg
-              aria-hidden="true"
-              className="w-6 h-6 ml-3"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
-                clipRule="evenodd"
-              ></path>
-            </svg>
-          </a>
-          <a
             href="https://honkai-star-rail.fandom.com/wiki/Trailblaze_EXP"
             target="_blank"
             className="inline-flex items-center justify-center p-5 text-base font-medium text-gray-500 rounded-lg bg-gray-50 hover:text-gray-900 hover:bg-gray-100 dark:text-gray-400 dark:bg-gray-800 dark:hover:bg-gray-700 dark:hover:text-white"
